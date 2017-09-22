42380f3e169822b3d7303c4b8166d2e532aa54de
Unknown <theosZA+github@googlemail.com>
2017-09-23 00:15:24 +0200

    Console solver clean-up

--- CubeConsole/main.cpp
+++ CubeConsole/main.cpp
@@ -7,44 +7,49 @@
 #include "..\Cube\3x3x3\SolverStep_2x2x3Block.h"
 #include "..\Cube\Scrambler\Scrambler.h"
 
-template <class Random>
-PartialSolution Solve2x2x2Random(SolverStep_2x2x2Block& solver, const std::vector<CubeMove>& scramble, Random& generator)
-{
-  auto solutions = solver.Solve(scramble, Solution{});
-  auto inverseSolutions = solver.Solve(InvertMoveSequence(scramble), Solution{});
-  for (auto& inverseSolution : inverseSolutions)
-  {
-    solutions.push_back(inverseSolution.Invert());
-  }
-  auto randomIndex = std::uniform_int_distribution<size_t>(0, solutions.size() - 1)(generator);
-  return solutions[randomIndex];
-}
-
-PartialSolution Solve2x2x3Best(SolverStep_2x2x3Block& solver, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
+std::vector<PartialSolution> GetAllPartialSolutions(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
 {
   // Rotate to canonical orientation.
   auto rotatedScramble = Rotate(scramble, partialSolution.rotationOldFaces, partialSolution.rotationNewFaces);
   auto rotatedSolutionSoFar = partialSolution.solutionSoFar.Rotate(partialSolution.rotationOldFaces, partialSolution.rotationNewFaces);
 
   // Get solutions.
-  auto rotatedSolutions = solver.Solve(rotatedScramble, rotatedSolutionSoFar);
-  auto inverseRotatedSolutions = solver.Solve(InvertMoveSequence(rotatedScramble), rotatedSolutionSoFar.Invert());
+  auto rotatedSolutions = solverStep.Solve(rotatedScramble, rotatedSolutionSoFar);
+  auto inverseRotatedSolutions = solverStep.Solve(InvertMoveSequence(rotatedScramble), rotatedSolutionSoFar.Invert());
   for (auto& inverseRotatedSolution : inverseRotatedSolutions)
   {
     rotatedSolutions.push_back(inverseRotatedSolution.Invert());
   }
-  
-  // Get best solution.
-  auto bestRotatedSolution = std::min_element(rotatedSolutions.begin(), rotatedSolutions.end(),
+
+  // Rotate back to original orientation.
+  std::vector<PartialSolution> solutions;
+  solutions.reserve(rotatedSolutions.size());
+  std::transform(rotatedSolutions.begin(), rotatedSolutions.end(), std::back_inserter(solutions),
+      [&](const PartialSolution& rotatedSolution)
+      {
+        return rotatedSolution.Rotate(partialSolution.rotationNewFaces, partialSolution.rotationOldFaces);
+      });
+  return solutions;
+}
+
+template <class Random>
+PartialSolution SolveRandom(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution, Random& generator)
+{
+  auto solutions = GetAllPartialSolutions(solverStep, scramble, partialSolution);
+  auto randomIndex = std::uniform_int_distribution<size_t>(0, solutions.size() - 1)(generator);
+  return solutions[randomIndex];
+
+}
+
+PartialSolution SolveBest(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
+{
+  auto solutions = GetAllPartialSolutions(solverStep, scramble, partialSolution);
+  return *std::min_element(solutions.begin(), solutions.end(),
       [](const PartialSolution& a, const PartialSolution& b)
       {
         return a.solutionSoFar.Length() < b.solutionSoFar.Length();
       });
-
-  // Rotate back to original orientation.
-  return bestRotatedSolution->Rotate(partialSolution.rotationNewFaces, partialSolution.rotationOldFaces);
 }
-
 
 int main()
 {
@@ -62,8 +67,8 @@ int main()
   {
     auto scramble = scrambler.CreateRandomScramble(25);
     
-    auto partialSolution = Solve2x2x2Random(solver2x2x2, scramble, generator);
-    auto solution = Solve2x2x3Best(solver2x2x3, scramble, partialSolution);
+    auto partialSolution = SolveRandom(solver2x2x2, scramble, PartialSolution{ Solution{}, CubeGroup::Scrambled }, generator);
+    auto solution = SolveBest(solver2x2x3, scramble, partialSolution);
 
     int stepLength = solution.solutionSoFar.Length() - partialSolution.solutionSoFar.Length();
     auto findIter = solveLengthCounts.find(stepLength);
