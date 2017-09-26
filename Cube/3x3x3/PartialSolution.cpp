73a47c7f6da3e6fd436b3f7fabf6c3a5d9274fe9
Unknown <theosZA+github@googlemail.com>
2017-09-26 11:53:45 +0200

    Fix for applying rotation to an already rotated partial solution

--- Cube/3x3x3/PartialSolution.cpp
+++ Cube/3x3x3/PartialSolution.cpp
@@ -18,21 +18,40 @@ PartialSolution::PartialSolution(Solution solutionSoFar, CubeGroup cubeGroup, st
 
 PartialSolution PartialSolution::Rotate(std::pair<Face, Face> oldFaces, std::pair<Face, Face> newFaces) const
 {
-  // Determine how all the old faces map to the new faces.
+  // We current need a rotation of (A,B)->(C,D) to orient correctly.
+  // We're rotating our current solution (W,X)->(Y,Z).
+  // So the new rotation that we need to orient correctly is (Y,Z)->(W,X) followed by (A,B)->(C,D)
+
+  // Create the mapping necessary to undo this new rotation. (Y,Z)->(W,X)
   auto oldFace3 = GetNextFaceClockwise(oldFaces.first, oldFaces.second);
   auto newFace3 = GetNextFaceClockwise(newFaces.first, newFaces.second);
-  std::array<Face, 6> newFace;
-  newFace[static_cast<size_t>(oldFaces.first)] = newFaces.first;
-  newFace[static_cast<size_t>(oldFaces.second)] = newFaces.second;
-  newFace[static_cast<size_t>(oldFace3)] = newFace3;
-  newFace[static_cast<size_t>(GetOppositeFace(oldFaces.first))] = GetOppositeFace(newFaces.first);
-  newFace[static_cast<size_t>(GetOppositeFace(oldFaces.second))] = GetOppositeFace(newFaces.second);
-  newFace[static_cast<size_t>(GetOppositeFace(oldFace3))] = GetOppositeFace(newFace3);
+  std::array<Face, 6> undoRotationFace;
+  undoRotationFace[static_cast<size_t>(newFaces.first)] = oldFaces.first;
+  undoRotationFace[static_cast<size_t>(newFaces.second)] = oldFaces.second;
+  undoRotationFace[static_cast<size_t>(newFace3)] = oldFace3;
+  undoRotationFace[static_cast<size_t>(GetOppositeFace(newFaces.first))] = GetOppositeFace(oldFaces.first);
+  undoRotationFace[static_cast<size_t>(GetOppositeFace(newFaces.second))] = GetOppositeFace(oldFaces.second);
+  undoRotationFace[static_cast<size_t>(GetOppositeFace(newFace3))] = GetOppositeFace(oldFace3);
+
+  // Create the mapping to apply our own rotation. (A,B)->(C,D)
+  auto ourOldFace3 = GetNextFaceClockwise(rotationOldFaces.first, rotationOldFaces.second);
+  auto ourNewFace3 = GetNextFaceClockwise(rotationNewFaces.first, rotationNewFaces.second);
+  std::array<Face, 6> thisRotationFace;
+  thisRotationFace[static_cast<size_t>(rotationOldFaces.first)] = rotationNewFaces.first;
+  thisRotationFace[static_cast<size_t>(rotationOldFaces.second)] = rotationNewFaces.second;
+  thisRotationFace[static_cast<size_t>(ourOldFace3)] = ourNewFace3;
+  thisRotationFace[static_cast<size_t>(GetOppositeFace(rotationOldFaces.first))] = GetOppositeFace(rotationNewFaces.first);
+  thisRotationFace[static_cast<size_t>(GetOppositeFace(rotationOldFaces.second))] = GetOppositeFace(rotationNewFaces.second);
+  thisRotationFace[static_cast<size_t>(GetOppositeFace(ourOldFace3))] = GetOppositeFace(ourNewFace3);
+
+  // Now determine the combined rotation.
+  auto frontMapsTo = thisRotationFace[static_cast<size_t>(undoRotationFace[static_cast<size_t>(Face::Front)])];
+  auto upMapsTo = thisRotationFace[static_cast<size_t>(undoRotationFace[static_cast<size_t>(Face::Up)])];
 
   return PartialSolution{ solutionSoFar.Rotate(oldFaces, newFaces),
-                          cubeGroup, 
-                          std::make_pair(newFace[static_cast<size_t>(rotationOldFaces.first)], newFace[static_cast<size_t>(rotationOldFaces.second)]),
-                          std::make_pair(newFace[static_cast<size_t>(rotationNewFaces.first)], newFace[static_cast<size_t>(rotationNewFaces.second)]) };
+                          cubeGroup,
+                          std::make_pair(Face::Front, Face::Up),
+                          std::make_pair(frontMapsTo, upMapsTo) };
 }
 
 PartialSolution PartialSolution::Invert() const
