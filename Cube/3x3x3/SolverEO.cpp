0e2b87ab1482dcf220eaffbf18561d835f554902
Unknown <theosZA+github@googlemail.com>
2017-09-26 12:21:42 +0200

    Added EO step as 1st step in Human Thistlethwaite

---
+++ Cube/3x3x3/SolverEO.cpp
@@ -0,0 +1,93 @@
+#include "SolverEO.h"
+
+#include <algorithm>
+#include <array>
+
+static const std::array<StickerPosition, 12> edgeCubies
+{
+  StickerPosition{ Face::Up, 1 },
+  StickerPosition{ Face::Up, 3 },
+  StickerPosition{ Face::Up, 5 },
+  StickerPosition{ Face::Up, 7 },
+  StickerPosition{ Face::Down, 1 },
+  StickerPosition{ Face::Down, 3 },
+  StickerPosition{ Face::Down, 5 },
+  StickerPosition{ Face::Down, 7 },
+  StickerPosition{ Face::Front, 3 },
+  StickerPosition{ Face::Front, 5 },
+  StickerPosition{ Face::Back, 3 },
+  StickerPosition{ Face::Back, 5 }
+};
+
+static const std::vector<CubeMove> possibleMoves
+{
+  CubeMove{ Face::Right, 1 },
+  CubeMove{ Face::Right, -1 },
+  CubeMove{ Face::Right, 2 },
+  CubeMove{ Face::Up, 1 },
+  CubeMove{ Face::Up, -1 },
+  CubeMove{ Face::Up, 2 },
+  CubeMove{ Face::Front, 1 },
+  CubeMove{ Face::Front, -1 },
+  CubeMove{ Face::Front, 2 },
+  CubeMove{ Face::Left, 1 },
+  CubeMove{ Face::Left, -1 },
+  CubeMove{ Face::Left, 2 },
+  CubeMove{ Face::Down, 1 },
+  CubeMove{ Face::Down, -1 },
+  CubeMove{ Face::Down, 2 },
+  CubeMove{ Face::Back, 2 },
+  CubeMove{ Face::Back, -1 },
+  CubeMove{ Face::Back, 1 }
+};
+
+SolverEO::SolverEO(std::uint32_t maxMoves, const std::string& cacheFileName)
+: solver(maxMoves,
+          [](const Cube3x3x3& cube)
+          {
+            return SolverEO::GetKeyValue(cube);
+          },
+          possibleMoves,
+          cacheFileName)
+{}
+
+std::vector<CubeMove> SolverEO::Solve(const Cube3x3x3& cube) const
+{
+  return solver.Solve(cube);
+}
+
+std::vector<CubeMove> SolverEO::Solve(const std::vector<CubeMove>& scramble) const
+{
+  Cube3x3x3 cube;
+  cube += scramble;
+  return Solve(cube);
+}
+
+std::uint32_t SolverEO::GetRequiredMoveCount(const Cube3x3x3& cube) const
+{
+  return solver.GetRequiredMoveCount(cube);
+}
+
+std::uint32_t SolverEO::GetRequiredMoveCount(const std::vector<CubeMove>& scramble) const
+{
+  Cube3x3x3 cube;
+  cube += scramble;
+  return GetRequiredMoveCount(cube);
+}
+
+std::uint32_t SolverEO::GetKeyValue(const Cube3x3x3& cube)
+{
+  // 12 edges, each:
+  //  1 bit = oriented correctly with respect to F/B
+  std::uint32_t key = 0;
+  for (const auto& edge : edgeCubies)
+  {
+    auto cubie = cube[edge];
+    if (std::find(edgeCubies.begin(), edgeCubies.end(), cubie) != edgeCubies.end())
+    {
+      key |= 1;
+    }
+    key <<= 1;
+  }
+  return key;
+}
---
+++ Cube/3x3x3/SolverStep_EO.cpp
@@ -0,0 +1,28 @@
+#include "SolverStep_EO.h"
+
+#include <array>
+
+SolverStep_EO::SolverStep_EO(const std::string& cacheFileName)
+: solverEO(20, cacheFileName)
+{}
+
+std::vector<PartialSolution> SolverStep_EO::Solve(const std::vector<CubeMove>& scramble, const Solution& solutionSoFar)
+{
+  std::vector<PartialSolution> partialSolutions;
+
+  // There are 3 possible orientations for doing edge orientation.
+  std::array<Face, 3> newFrontFaces{ Face::Front, Face::Right, Face::Down };
+  std::array<Face, 3> newUpFaces{ Face::Up, Face::Up, Face::Front };
+  for (size_t i = 0; i < 3; ++i)
+  {
+    auto rotatedScramble = Rotate(scramble, std::make_pair(Face::Front, Face::Up), std::make_pair(newFrontFaces[i], newUpFaces[i]));
+    auto rotatedSolutionSoFar = solutionSoFar.Rotate(std::make_pair(Face::Front, Face::Up), std::make_pair(newFrontFaces[i], newUpFaces[i]));
+    auto rotatedScramblePlusSolution = rotatedSolutionSoFar.CombineScrambleAndSolution(rotatedScramble);
+    auto rotatedStep = SolutionStep{ "EO", solverEO.Solve(rotatedScramblePlusSolution) };
+    auto rotatedPartialSolution = rotatedSolutionSoFar + rotatedStep;
+    auto partialSolution = rotatedPartialSolution.Rotate(std::make_pair(newFrontFaces[i], newUpFaces[i]), std::make_pair(Face::Front, Face::Up));
+    partialSolutions.push_back(PartialSolution{ partialSolution, CubeGroup::EO, std::make_pair(Face::Front, Face::Up), std::make_pair(newFrontFaces[i], newUpFaces[i]) });
+  }
+
+  return partialSolutions;
+}
