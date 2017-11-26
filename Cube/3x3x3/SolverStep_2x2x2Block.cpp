f2dd22ce44a1b21588872e0e4758aae9d18f4e20
Unknown <theosZA+github@googlemail.com>
2017-11-26 15:57:33 +0200

    Allow a pre-move for 2x2x2 block (i.e. a pseudo-block)

--- Cube/3x3x3/SolverStep_2x2x2Block.cpp
+++ Cube/3x3x3/SolverStep_2x2x2Block.cpp
@@ -21,6 +21,13 @@ std::vector<PartialSolution> SolverStep_2x2x2Block::Solve(const std::vector<Cube
     std::pair<Face,Face>{ Face::Right, Face::Front },
     std::pair<Face,Face>{ Face::Back, Face::Right }
   };
+  
+  // There are 9 possible pre-moves.
+  const std::array<CubeMove, 9> preMoves{
+    CubeMove{ Face::Back, 1 }, CubeMove{ Face::Back, -1 }, CubeMove{ Face::Back, 2 },
+    CubeMove{ Face::Left, 1 }, CubeMove{ Face::Left, -1 }, CubeMove{ Face::Left, 2 },
+    CubeMove{ Face::Down, 1 }, CubeMove{ Face::Down, -1 }, CubeMove{ Face::Down, 2 },
+  };
 
   for (const auto& corner : corners)
   {
@@ -29,6 +36,20 @@ std::vector<PartialSolution> SolverStep_2x2x2Block::Solve(const std::vector<Cube
     auto rotatedPartialSolution = Solution{} + rotatedStep;
     auto partialSolution = rotatedPartialSolution.Rotate(std::make_pair(Face::Back, Face::Left), corner);
     partialSolutions.push_back(PartialSolution{ partialSolution, CubeGroup::Block2x2x2, corner, std::make_pair(Face::Back, Face::Left) });
+
+    // Consider all pre-moves.
+    for (const auto& preMove : preMoves)
+    {
+      auto rotatedScrambleWithPreMove = std::vector<CubeMove>{ preMove } + rotatedScramble;
+      auto stepMoves = solver2x2x2.Solve(rotatedScrambleWithPreMove, Face::Back, Face::Left);
+      std::vector<bool> isStepMoveOnInverseSolve(stepMoves.size(), false);
+      stepMoves.push_back(InvertMove(preMove));
+      isStepMoveOnInverseSolve.push_back(true); // only the pre-move is on the inverse solve
+      auto rotatedStep = SolutionStep{ "2x2x2", stepMoves, isStepMoveOnInverseSolve };
+      auto rotatedPartialSolution = Solution{} + rotatedStep;
+      auto partialSolution = rotatedPartialSolution.Rotate(std::make_pair(Face::Back, Face::Left), corner);
+      partialSolutions.push_back(PartialSolution{ partialSolution, CubeGroup::Block2x2x2, corner, std::make_pair(Face::Back, Face::Left) });
+    }
   }
 
   return partialSolutions;
