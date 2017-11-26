10511876f6d6dc4d7a909336ffc10e29b6ef77c4
Unknown <theosZA+github@googlemail.com>
2017-11-26 15:56:59 +0200

    Random scrambles now simplified

--- Cube/Scrambler/Scrambler.cpp
+++ Cube/Scrambler/Scrambler.cpp
@@ -8,16 +8,15 @@ std::vector<CubeMove> Scrambler::CreateRandomScramble(size_t scrambleLength)
 {
   std::uniform_int_distribution<> faceDistribution(0, 5);
   std::uniform_int_distribution<> rotationDistribution(0, 2);
-  std::vector<CubeMove> scramble(scrambleLength);
-  for (size_t i = 0; i < scrambleLength; ++i)
+  std::vector<CubeMove> scramble;
+  while (scramble.size() < scrambleLength)
   {
-    do
-      scramble[i].face = static_cast<Face>(faceDistribution(generator));
-    while (i > 0 && scramble[i].face == scramble[i - 1].face);
-
-    scramble[i].quarterRotationsClockwise = rotationDistribution(generator);
-    if (scramble[i].quarterRotationsClockwise == 0)
-      scramble[i].quarterRotationsClockwise = -1;
+    auto face = static_cast<Face>(faceDistribution(generator));
+    auto rotations = rotationDistribution(generator);
+    if (rotations == 0)
+      rotations = -1;
+    scramble.push_back(CubeMove{ face, rotations });
+    scramble = SimplifyMoveSequence(scramble);
   }
 
   return scramble;
