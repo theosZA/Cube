f5157de26d0c6a003053edabc23252811bc01e56
Unknown <theosZA+github@googlemail.com>
2017-12-02 15:02:26 +0200

    Move solving logic into AnalysisSolver with an A* search

---
+++ CubeConsole/AnalysisSolver.cpp
@@ -0,0 +1,100 @@
+#include "AnalysisSolver.h"
+
+#include <queue>
+#include <stdexcept>
+
+#include "..\Cube\3x3x3\PartialSolution.h"
+#include "..\Cube\3x3x3\SolverStep_2x2x2Block.h"
+#include "..\Cube\3x3x3\SolverStep_2x2x3Block.h"
+
+std::vector<PartialSolution> GetAllPartialSolutions(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
+{
+  // Rotate to canonical orientation.
+  auto rotatedScramble = Rotate(scramble, partialSolution.rotationOldFaces, partialSolution.rotationNewFaces);
+  auto rotatedSolutionSoFar = partialSolution.solutionSoFar.Rotate(partialSolution.rotationOldFaces, partialSolution.rotationNewFaces);
+
+  // Get solutions.
+  auto rotatedSolutions = solverStep.Solve(rotatedScramble, rotatedSolutionSoFar);
+  auto inverseRotatedSolutions = solverStep.Solve(InvertMoveSequence(rotatedScramble), rotatedSolutionSoFar.Invert());
+  for (auto& inverseRotatedSolution : inverseRotatedSolutions)
+  {
+    rotatedSolutions.push_back(inverseRotatedSolution.Invert());
+  }
+
+  // Rotate back to original orientation.
+  std::vector<PartialSolution> solutions;
+  solutions.reserve(rotatedSolutions.size());
+  std::transform(rotatedSolutions.begin(), rotatedSolutions.end(), std::back_inserter(solutions),
+    [&](const PartialSolution& rotatedSolution)
+    {
+      return rotatedSolution.Rotate(partialSolution.rotationNewFaces, partialSolution.rotationOldFaces);
+    });
+  return solutions;
+}
+
+template <class Random>
+PartialSolution SolveRandom(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution, Random& generator)
+{
+  auto solutions = GetAllPartialSolutions(solverStep, scramble, partialSolution);
+  auto randomIndex = std::uniform_int_distribution<size_t>(0, solutions.size() - 1)(generator);
+  return solutions[randomIndex];
+}
+
+PartialSolution SolveBest(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
+{
+  auto solutions = GetAllPartialSolutions(solverStep, scramble, partialSolution);
+  return *std::min_element(solutions.begin(), solutions.end(),
+    [](const PartialSolution& a, const PartialSolution& b)
+  {
+    return a.solutionSoFar.Length() < b.solutionSoFar.Length();
+  });
+}
+
+AnalysisSolver::AnalysisSolver()
+{
+  solverSteps[CubeGroup::Scrambled].reset(new SolverStep_2x2x2Block("block2x2x2.3x3"));
+  solverSteps[CubeGroup::Block2x2x2].reset(new SolverStep_2x2x3Block("block2x2x3.3x3"));
+}
+
+void AnalysisSolver::SetScramble(const std::vector<CubeMove>& scramble)
+{
+  this->scramble = scramble;
+}
+
+Solution AnalysisSolver::BestSolve(CubeGroup targetState)
+{
+  auto priorityFunction = [](const PartialSolution& a, const PartialSolution& b)
+  {
+    return a.solutionSoFar.Length() + EstimateMovesRequired(a.cubeGroup) > b.solutionSoFar.Length() + EstimateMovesRequired(b.cubeGroup);
+  };
+  std::priority_queue<PartialSolution, std::vector<PartialSolution>, decltype(priorityFunction)> partialSolutions(priorityFunction);
+  partialSolutions.push(PartialSolution{ Solution{}, CubeGroup::Scrambled });
+  
+  while (!partialSolutions.empty() && partialSolutions.top().cubeGroup != targetState)
+  {
+    auto currentPartialSolution = partialSolutions.top();
+    partialSolutions.pop();
+    
+    // Get the solver step for this state. If we don't have a solver for it, then skip it.
+    auto findIter = solverSteps.find(currentPartialSolution.cubeGroup);
+    if (findIter == solverSteps.end())
+    {
+      continue;
+    }
+    auto& solverStep = *findIter->second;
+
+    // Now solve to get all successor states and add them to our priority queue.
+    auto successorPartialSolutions = GetAllPartialSolutions(solverStep, scramble, currentPartialSolution);
+    for (const auto& partialSolution : successorPartialSolutions)
+    {
+      partialSolutions.push(partialSolution);
+    }
+  }
+
+  if (partialSolutions.empty())
+  {
+    throw std::runtime_error("Failed to find solution");
+  }
+
+  return partialSolutions.top().solutionSoFar;
+}
--- CubeConsole/main.cpp
+++ CubeConsole/main.cpp
@@ -1,95 +1,55 @@
-#include <algorithm>
-#include <iostream>
 #include <map>
+#include <iostream>
 #include <random>
+#include <stdexcept>
 
-#include "..\Cube\3x3x3\SolverStep_2x2x2Block.h"
-#include "..\Cube\3x3x3\SolverStep_2x2x3Block.h"
-#include "..\Cube\3x3x3\SolverStep_2x2x3EO.h"
-#include "..\Cube\3x3x3\SolverStep_EO.h"
-#include "..\Cube\3x3x3\SolverStep_DominoReduction.h"
-#include "..\Cube\3x3x3\SolverStep_DominoToDoubleTurns.h"
+#include "..\Cube\3x3x3\CubeGroup.h"
 #include "..\Cube\Scrambler\Scrambler.h"
 
-std::vector<PartialSolution> GetAllPartialSolutions(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
-{
-  // Rotate to canonical orientation.
-  auto rotatedScramble = Rotate(scramble, partialSolution.rotationOldFaces, partialSolution.rotationNewFaces);
-  auto rotatedSolutionSoFar = partialSolution.solutionSoFar.Rotate(partialSolution.rotationOldFaces, partialSolution.rotationNewFaces);
-
-  // Get solutions.
-  auto rotatedSolutions = solverStep.Solve(rotatedScramble, rotatedSolutionSoFar);
-  auto inverseRotatedSolutions = solverStep.Solve(InvertMoveSequence(rotatedScramble), rotatedSolutionSoFar.Invert());
-  for (auto& inverseRotatedSolution : inverseRotatedSolutions)
-  {
-    rotatedSolutions.push_back(inverseRotatedSolution.Invert());
-  }
-
-  // Rotate back to original orientation.
-  std::vector<PartialSolution> solutions;
-  solutions.reserve(rotatedSolutions.size());
-  std::transform(rotatedSolutions.begin(), rotatedSolutions.end(), std::back_inserter(solutions),
-      [&](const PartialSolution& rotatedSolution)
-      {
-        return rotatedSolution.Rotate(partialSolution.rotationNewFaces, partialSolution.rotationOldFaces);
-      });
-  return solutions;
-}
-
-template <class Random>
-PartialSolution SolveRandom(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution, Random& generator)
-{
-  auto solutions = GetAllPartialSolutions(solverStep, scramble, partialSolution);
-  auto randomIndex = std::uniform_int_distribution<size_t>(0, solutions.size() - 1)(generator);
-  return solutions[randomIndex];
-}
-
-PartialSolution SolveBest(SolverStep& solverStep, const std::vector<CubeMove>& scramble, const PartialSolution& partialSolution)
-{
-  auto solutions = GetAllPartialSolutions(solverStep, scramble, partialSolution);
-  return *std::min_element(solutions.begin(), solutions.end(),
-      [](const PartialSolution& a, const PartialSolution& b)
-      {
-        return a.solutionSoFar.Length() < b.solutionSoFar.Length();
-      });
-}
+#include "AnalysisSolver.h"
 
 int main()
 {
-  std::random_device randomSource;
-  Scrambler scrambler(randomSource());
-
-  std::mt19937 generator;
-  
-  std::map<int, int> solveLengthCounts;
-  SolverStep_2x2x2Block solver2x2x2("block2x2x2.3x3");
-
-  for (size_t i = 0; i < 100000; ++i)
+  try
   {
-    auto scramble = scrambler.CreateRandomScramble(25);
-    
-    auto solution = SolveBest(solver2x2x2, scramble, PartialSolution{ Solution{}, CubeGroup::Scrambled });
+    std::random_device randomSource;
+    Scrambler scrambler(randomSource());
 
-    int stepLength = solution.solutionSoFar.Length();
-    auto findIter = solveLengthCounts.find(stepLength);
-    if (findIter == solveLengthCounts.end())
-    {
-      solveLengthCounts.insert(std::make_pair(stepLength, 1));
-    }
-    else
-    {
-      ++(findIter->second);
-    }
+    std::mt19937 generator;
+  
+    std::map<int, int> solveLengthCounts;
+    AnalysisSolver solver;
 
-    if (i % 1000 == 0)
+    for (size_t i = 0; i < 100; ++i)
     {
-      std::cout << i << ' ';
+      solver.SetScramble(scrambler.CreateRandomScramble(25));
+      auto solution = solver.BestSolve(CubeGroup::Block2x2x3);
+
+      int stepLength = solution.Length();
+      auto findIter = solveLengthCounts.find(stepLength);
+      if (findIter == solveLengthCounts.end())
+      {
+        solveLengthCounts.insert(std::make_pair(stepLength, 1));
+      }
+      else
+      {
+        ++(findIter->second);
+      }
+
+      if (i % 1000 == 0)
+      {
+        std::cout << i << ' ';
+      }
+    }
+    std::cout << "\n\n";
+
+    for (const auto& solveLengthCount : solveLengthCounts)
+    {
+      std::cout << solveLengthCount.first << ": " << solveLengthCount.second << '\n';   
     }
   }
-  std::cout << "\n\n";
-
-  for (const auto& solveLengthCount : solveLengthCounts)
+  catch (const std::exception& e)
   {
-    std::cout << solveLengthCount.first << ": " << solveLengthCount.second << '\n';   
+    std::cerr << e.what() << '\n';
   }
 }
