07316ff10146cb1f8556026dc2240514b9460afd
Unknown <theosZA+github@googlemail.com>
2018-03-26 20:17:51 +0200

    Cube API node.js project

---
+++ CubeApi/app.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', function (req, res) {
+  res.send('Use POST /solve');
+});
+
+app.post('/solve', function (req, res) {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  res.send('Scramble: ' + req.body.scramble);
+});
+
+// Development error handler - will print stack trace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler - no stack traces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
